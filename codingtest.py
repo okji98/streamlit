40095ac7907cea071b7f58005cd69edae2d25641
okji98 <dnflsmstltk10@gmail.com>
2025-03-24 21:06:12 +0900

    코딩 연습

--- codingtest.py
+++ codingtest.py
@@ -1,16 +1,8 @@
-my_string = "He11oWor1d"
-overwrite_string = "lloWorl"
-s = 2
-
-# def solution(my_string, overwrite_string, s):
-#     answer = ''
-#     return answer
-print(my_string[s:])
-print(len(overwrite_string))
-print(my_string[s:len(overwrite_string) + 2])
-# ov_len = len(overwrite_string)
-my_string = my_string.replace(my_string[s:len(overwrite_string) + 2], overwrite_string)
-print(my_string)
-# while True:
-#     if len(overwrite_string)
-#     my_string[s:] = overwrite_string
+>>> a = [1, 2, 3]
+>>> a.extend([4, 5])
+>>> a
+[1, 2, 3, 4, 5]
+>>> b = [6, 7]
+>>> a.extend(b)
+>>> a
+[1, 2, 3, 4, 5, 6, 7]
